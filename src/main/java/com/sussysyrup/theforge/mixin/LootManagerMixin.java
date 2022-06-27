014abc46efc9f1cb41df7ee6d15204c2c5e31ffe
papa-noobus <96626528+papa-noobus@users.noreply.github.com>
2022-06-27 22:20:31 +0100

    Update LootManagerMixin.java

--- src/main/java/com/sussysyrup/theforge/mixin/LootManagerMixin.java
+++ src/main/java/com/sussysyrup/theforge/mixin/LootManagerMixin.java
@@ -1,27 +1,33 @@
 package com.sussysyrup.theforge.mixin;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.JsonElement;
 import com.sussysyrup.theforge.api.block.ForgePartBenchRegistry;
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.minecraft.loot.LootManager;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Mixin(LootManager.class)
 public class LootManagerMixin {
 
     //Injecting custom loot tables here based on runtime datagen. This is done before the usual processing and hence should be overrideable
-    @Inject(method = "apply", at = @At(value = "INVOKE", target = "Ljava/util/Map;remove(Ljava/lang/Object;)Ljava/lang/Object;"))
-    public void apply()
+    @Inject(method = "apply", at = @At(value = "INVOKE", target = "Ljava/util/Map;remove(Ljava/lang/Object;)Ljava/lang/Object;"), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void apply(Map<Identifier, JsonElement> map, ResourceManager resourceManager, Profiler profiler, CallbackInfo callbackInfo, ImmutableMap.Builder builderIn)
     {
         HashMap<Identifier, Identifier> poolIDMap = new HashMap<>();
         List<Identifier> list = ForgePartBenchRegistry.getPartBenchWoodMap().keySet().stream().toList();
@@ -42,7 +48,8 @@ public class LootManagerMixin {
 
             LootTable lootTable = builder.build();
 
-            //map.put(id, lootTable);
+            builderIn.put(id, lootTable);
         }
     }
+
 }
